e541512ac61b6a299386e691a53dfd8f90c63bd6
Chris Li <top770977@163.com>
2018-08-30 21:01:45 +0800

    20180830 2101

---
+++ java1408/src/java1408.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * FileName: java1408
+ * Author:   Chris Li
+ * Date:     8/30/2018 7:41 PM
+ * Description: java1408
+ * History:
+ */
+
+public class java1408 {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+
+    }
+}
